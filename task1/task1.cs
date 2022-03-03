f1a56d0af4e92c57abfd3a170ef806adad80af9b
Spoon2 <viktor.spoon2@gmail.com>
2022-03-03 15:44:16 +0400

    task1 created

---
+++ task1/task1.cs
@@ -0,0 +1,8 @@
+﻿
+Console.WriteLine("Программа для определения второй цифры трехзначного числа");
+
+Console.WriteLine("Введите трехзначное число");
+
+string i = Convert.ToString(Console.ReadLine());
+
+Console.WriteLine(i[1]+ " - вторая цифра заданного числа");
