50247baf7a42b03d45ef6594f036a1b6a6c5bb99
Spoon2 <viktor.spoon2@gmail.com>
2022-03-04 16:06:07 +0400

    task2 created

---
+++ task2/task2.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Программа для удаления второй цифры случайного трехзначного числа");
+
+Random rnd = new Random();
+
+int i = rnd.Next(100,1000);
+
+Console.WriteLine(i);
+string j = Convert.ToString(i);
+
+j = j.Remove(1,1);
+
+Console.WriteLine(j);
