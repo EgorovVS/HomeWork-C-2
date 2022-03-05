5166368b640cee003335c1258624bff688199739
Spoon2 <viktor.spoon2@gmail.com>
2022-03-05 16:59:42 +0400

    task4 created

---
+++ task4/task4.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine("Программа определения выходного дня");
+
+Console.WriteLine("Введите номер дня недели");
+int dayNum = Convert.ToInt32(Console.ReadLine());
+if(dayNum>7){
+    Console.WriteLine("В неделе 7 дней!!");
+}
+if(dayNum>5&dayNum<8){
+Console.WriteLine("Да");
+}
+else {
+Console.WriteLine("Нет");
+}
+   
+    
