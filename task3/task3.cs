e7363ed5b12d9f427c5c467a6f43817ab8355d25
Spoon2 <viktor.spoon2@gmail.com>
2022-03-05 13:56:55 +0400

    task3 created

---
+++ task3/task3.cs
@@ -0,0 +1,10 @@
+﻿
+Console.WriteLine("Программа вывода третьей цифры заданного числа");
+
+Console.WriteLine("Введите число");
+
+string i = Convert.ToString(Console.ReadLine());
+
+if(i.Length>2){
+Console.WriteLine(i[2] + " - третья цифра заданного числа");
+} else { Console.WriteLine("Третьей цифры нет");}
